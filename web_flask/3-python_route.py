c4bfeccbbb489a2100bf869c33eac4bb76b528a6
tennin12 <bassembenyhia12@gmail.com>
2021-09-11 21:18:48 +0100

    f3 new fix

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -35,6 +35,7 @@ def python(text='is cool'):
     """ outputs 'Python <text>'
         uses 'is cool' if <text> not supplied
     """
+    text = text.replace('_', ' ')
 
     app.url_map.strict_slashes = False
     return ('Python {:s}'.format(text))
