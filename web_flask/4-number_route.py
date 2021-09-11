d32c3c6ab4f8d0751035e8fd5a9c8d92ebd5f553
tennin12 <bassembenyhia12@gmail.com>
2021-09-11 21:36:05 +0100

    f4 added

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+3-python_route.py - starts a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    """ outputs 'Hello HBNB!' """
+    app.url_map.strict_slashes = False
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ outputs 'HBNB' """
+    app.url_map.strict_slashes = False
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    """ outputs 'C <text> """
+    text = text.replace('_', ' ')
+
+    app.url_map.strict_slashes = False
+    return 'C {}'.format(text)
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    """ outputs 'Python <text>'
+        uses 'is cool' if <text> not supplied
+    """
+    text = text.replace('_', ' ')
+
+    app.url_map.strict_slashes = False
+    return ('Python {:s}'.format(text))
+
+
+@app.route('/number/<n>')
+def python(n):
+    """
+    output integer
+    """
+    app.url_map.strict_slashes = False
+    return ('{} is a number'.format(n))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
