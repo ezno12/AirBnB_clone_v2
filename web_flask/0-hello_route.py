bcf25a5f04af7914dfe79e678380c358da246aa6
tennin12 <bassembenyhia12@gmail.com>
2021-09-11 18:12:34 +0100

    f0 fixed

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-
-
+"""
+0-hell_route - starts a Flask web app
+"""
 from flask import Flask
 app = Flask(__name__)
-    
+
+
 @app.route('/')
 def hello_hbnb():
     """ outputs 'Hello HBNB!' """
     app.url_map.strict_slashes = False
     return 'Hello, HBNB!'
-
 
 if __name__ == '__main__':
     app.debug = True
