eadb2e3febc87d88e48c0dd61fd22a74db386015
tennin12 <bassembenyhia12@gmail.com>
2021-09-12 17:46:36 +0100

    state file updated

--- models/state.py
+++ models/state.py
@@ -1,8 +1,38 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""
+State Class from Models Module
+"""
+
+from models.base_model import BaseModel, Base
+import models
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
+from os import environ, getenv
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """State class handles all application states"""
+
+    if 'HBNB_TYPE_STORAGE' in environ and environ['HBNB_TYPE_STORAGE'] == 'db':
+
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete-orphan",
+                              backref="state")
+    else:
+        name = ""
+
+    if (getenv('HBNB_TYPE_STORAGE') != 'db'):
+        @property
+        def cities(self):
+            """ returns all city objects  associated with this State  """
+
+            cities = models.storage.all('City').values()
+
+            results = [city for city in cities if city.state_id == self.id]
+
+            return (results)
+
+    def __init__(self, *args, **kwargs):
+        """instantiates a new state"""
+        super().__init__(self, *args, **kwargs)
